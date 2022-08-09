b3381ee791a538e29fbcd15080412019567b60bd
VanessaTocasuche <vanetocasuche@gmail.com>
2022-08-09 01:03:44 -0500

    navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -20,23 +20,25 @@ function Navbar() {
         </Link>
       </div>
       <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <ul className='nav-menu-items' onClick={showSidebar}>
-          <li className='navbar-toggle'>
-            <Link to='#' className='menu-bars'>
-              <AiIcons.AiOutlineClose />
-            </Link>
-          </li>
-          {SidebarDatos.map((item) => {
-            return (
-              <li key={item.index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+        <button type='button' onClick={showSidebar} className='nav-menu-items'>
+          <ul>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarDatos.map((item) => {
+              return (
+                <li key={item.index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </button>
       </nav>
     </IconContext.Provider>
   );
